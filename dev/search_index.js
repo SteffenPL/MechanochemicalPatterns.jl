bf43fb6f29d0a32ea4a66508ac608b6d20740723
Documenter.jl <documenter@juliadocs.github.io>
2023-11-08 02:02:30 +0000

    build based on 7a9ef7a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MechanochemicalPatterns","category":"page"},{"location":"#MechanochemicalPatterns","page":"Home","title":"MechanochemicalPatterns","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MechanochemicalPatterns.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MechanochemicalPatterns]","category":"page"}]
+}
