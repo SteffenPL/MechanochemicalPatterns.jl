02e7f4c5a12dec8ebcba7e3abc9e6b2c9c8c85e4
Steffen Plunder <steffen.plunder@web.de>
2023-11-08 14:23:20 +0900

    cleanup

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://SteffenPL.github.io/MechanochemicalPatterns.jl/dev/)
 [![Build Status](https://github.com/SteffenPL/MechanochemicalPatterns.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/SteffenPL/MechanochemicalPatterns.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/SteffenPL/MechanochemicalPatterns.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/SteffenPL/MechanochemicalPatterns.jl)
+
+
+## Setup
+
+Use the usual approach to `instantiate` the Julia environment.
+```julia
+using Pkg
+Pkg.instantiate()  # or in pkg> mode
+```
+
+In addition, there is the file `scripts\config.toml` which is not shipped with the package
+for local configuration (such as paths). Copy the file to `scripts\config_template.toml` to `scripts\config.toml` and edit the file to your needs.
+
+The `data_folder` (see config file) refers to a shared folder which collects all 
+experimental datasets. As these files are large we use other cloud drives to share these datasets (Google Drive or OneDrive).
--- scripts/import_tracking_data.jl
+++ scripts/import_tracking_data.jl
@@ -1,7 +1,9 @@
-using CSV, DataFrames, Glob, TOML
+using CSV, DataFrames, Glob, TOML, GLMakie
 config = TOML.parsefile("scripts/config.toml")
 
+config["makie_inline"] && Makie.inline!(true) 
 
+# real all tracking data from given folder (inside data_dir)
 csv_files = readdir(glob"*/FW_Day1_Cycle_01/*.csv", config["data_dir"])
 df = CSV.read(csv_files[2], DataFrame, skipto = 5)
 
@@ -10,22 +12,14 @@ sort!(df, :POSITION_T)
 last_frame = maximum(df.FRAME)
 filter!( row -> row.FRAME < last_frame, df)
 
-using GLMakie
-begin 
-    fig = Figure()
-    ax = Axis3(fig[1,1])
-
-    # for label in unique(df[:, :ID])
-    #     df_label = filter(row -> row.ID == label, df)
-    #     lines!(ax, df_label[:, :POSITION_X], df_label[:, :POSITION_Y], df_label[:, :POSITION_Z], label=label)
-    # end
-
-    for (label, df_label) in pairs(groupby(df, :TRACK_ID))
-        lines!(ax, df_label.POSITION_X, df_label.POSITION_Y, df_label.POSITION_Z, label=label)
-    end
-
-    df_init = filter(row -> row.POSITION_T == 0, df)
-    scatter!(ax, df_init.POSITION_X, df_init.POSITION_Y, df_init.POSITION_Z, markersize = 50^2, color = :black, label = "init")
-    
-    fig 
+# plot 3D trajectories
+fig = Figure()
+ax = Axis3(fig[1,1])
+for (label, df_label) in pairs(groupby(df, :TRACK_ID))
+    lines!(ax, df_label.POSITION_X, df_label.POSITION_Y, df_label.POSITION_Z, label=label)
 end
+
+df_init = filter(row -> row.POSITION_T == 0, df)
+scatter!(ax, df_init.POSITION_X, df_init.POSITION_Y, df_init.POSITION_Z, markersize = 50^2, color = :black, label = "init")
+
+display(fig) 
